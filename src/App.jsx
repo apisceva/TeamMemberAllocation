6665f1052e6bffdc9c40577561059463e6329ac8
apisceva <aljona.gromova@gmail.com>
2022-09-19 09:34:06 -0400

    added App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer";
+import Employees from "./Employees";
 
 function App() {
   return (
     <div>
       <Header />
+      <Employees />
+      <Footer />
     </div>
   );
 }
